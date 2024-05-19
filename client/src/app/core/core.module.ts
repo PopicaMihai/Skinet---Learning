eba7045ef3a8da4fbad6c05ec2026c1208eafc5b
Mihai <popmihai2707@gmail.com>
2024-05-20 00:45:24 +0300

    Added routing + lazy loading

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     CommonModule,
+    RouterModule
   ],
   exports: [
     NavbarComponent
